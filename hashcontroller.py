ffe292379ab263284a63de807272b5fbb51af626
FrancescoDiSalesGithub <f.disales@hotmail.com>
2022-02-13 01:34:57 +0100

    adding import/export function

---
+++ hashcontroller.py
@@ -0,0 +1,97 @@
+
+import hashlib
+import sqlite3
+
+class hashcontroller:
+    def __init__(self,file) -> None:
+        self.file = file
+        pass
+
+    def generate_md5_sql(self):
+        
+
+                file_md5=open(self.file,"r",encoding='utf-8',errors='ignore')
+                file_md5_sql=open("md5toinsert.sql","w",errors='ignore')
+
+                for value in file_md5:
+                    md5 = hashlib.md5()
+                
+                    md5.update(value.encode(encoding='utf-8'))
+                    hashdone = md5.hexdigest()
+                    file_md5_sql.write("\n")
+                    file_md5_sql.write("insert into md5 values ('{}','{}');".format(str(hashdone),str(value).rstrip()))
+
+                file_md5.close()
+                print("export done!,check md5toinsert.sql")
+
+
+    def generate_sha512_sql(self):
+
+        file_sha512 = open(self.file,"r",encoding='utf-8',errors='ignore')
+        file_sha512_sql = open("sha512toinsert.sql","w",errors='ignore')
+
+        for value in file_sha512:
+            sha512 = hashlib.sha512()
+            sha512.update(value.encode(encoding='utf-8'))
+            sha512done = sha512.hexdigest()
+
+            file_sha512_sql.write("\n")
+            file_sha512_sql.write("insert into sha512 values ('{}','{}');\n".format(str(sha512done),str(sha512done).rstrip()))
+
+        file_sha512_sql.close()
+        print("export done!,check sha512toinsert.sql")
+
+        
+
+    def generate_sha256_sql(self):
+        file_sha256 = open(self.file,"r",encoding='utf-8',errors='ignore')
+        file_sha256_sql = open("sha256toinsert.sql","w",errors='ignore')
+        
+        for value in file_sha256:
+            sha256 = hashlib.sha256()
+            sha256.update(value.encode(encoding='utf-8'))
+            sha256done = sha256.hexdigest()
+
+            file_sha256_sql.write("\n")
+            file_sha256_sql.write("insert into sha256 values ('{}','{}');\n".format(str(sha256done),str(value).rstrip()))
+
+        file_sha256_sql.close()
+        print("export done!,check sha256toinsert.sql")
+
+    def import_md5_sql(self):
+        file_md5 = open(self.file,"r",encoding='utf-8',errors='ignore')
+        db_connection = sqlite3.connect("rainbowtables.db")
+
+        for value in file_md5:
+            value.encode('utf-8',errors='ignore')
+            db_connection.execute(value)
+            db_connection.commit()
+            
+        db_connection.close()
+        file_md5.close()
+
+
+    def import_sha256_sql(self):
+        file_sha256 = open(self.file,"r",encoding='utf-8',errors='ignore')
+        db_connection = sqlite3.connect("rainbowtables.db")
+
+        for value in file_sha256:
+            value.encode('utf-8',errors='ignore')
+            db_connection.execute(value)
+            db_connection.commit()
+            
+        db_connection.close()
+        file_sha256.close()
+
+
+    def import_sha512_sql(self):
+        file_sha512 = open(self.file,"r",encoding='utf-8',errors='ignore')
+        db_connection = sqlite3.connect("rainbowtables.db")
+
+        for value in file_sha512:
+            value.encode('utf-8',errors='ignore')
+            db_connection.execute(str(value))
+            db_connection.commit()
+            
+        db_connection.close()
+        file_sha512.close()
--- rainbowRain.py
+++ rainbowRain.py
@@ -1,25 +1,74 @@
 import sys
 import sqlite3
-import yaml
-import databaseobj
-import json
 
+import hashcontroller
 
 if __name__ == "__main__":
     
-        con = sqlite3.connect("rainbowtables.db")
+           
+        
+            if(sys.argv[1] == "md5"):
+                con = sqlite3.connect("rainbowtables.db")
+                cursor = con.cursor()
+                cursor.execute("SELECT * from md5 where hash = ?",(sys.argv[2],))
+                rows = cursor.fetchall()
 
-        if(sys.argv[1] == "md5"):
-            for row in con.execute("SELECT * from md5 where hash = ?",(sys.argv[2],)):
-                print(row)
+                for row in rows:
+                    print(row)
 
-        if(sys.argv[1] == "sha512"):
-            for row in con.execute("SELECT * from sha512 where hash = ?",(sys.argv[2],)):
-                print(row)
+                con.close()
 
-        con.close()
-   
-    
+            if(sys.argv[1] == "sha512"):
+                print("argv1:"+sys.argv[1])
+                print("argv2:"+sys.argv[2])
+
+                con = sqlite3.connect("rainbowtables.db")
+                cursor = con.cursor()
+                cursor.execute("SELECT * from sha512 where hash = ?",(sys.argv[2],))
+                rows = cursor.fetchall()
+
+                for row in rows:
+                    print(row)
+                con.close()
+                
+
+            if(sys.argv[1] == "sha256"):
+                con = sqlite3.connect("rainbowtables.db")
+                cursor = con.cursor()
+                cursor.execute("SELECT * from sha256 where hash = ?",(sys.argv[2],))
+                rows = cursor.fetchall()
+
+                for row in rows:
+                    print(row)
+
+                con.close()
+
+            if(sys.argv[1]=="exportmd5"):
+                hash_md5_export = hashcontroller.hashcontroller(str(sys.argv[2]))
+                hash_md5_export.generate_md5_sql()
+
+            if(sys.argv[1]=="exportsha256"):
+                hash_sha_256_export = hashcontroller.hashcontroller(str(sys.argv[2]))
+                hash_sha_256_export.generate_sha256_sql()
+
+            if(sys.argv[1]=="exportsha512"):
+                hash_sha_512_export = hashcontroller.hashcontroller(str(sys.argv[2]))
+                hash_sha_512_export.generate_sha512_sql()    
+
+            if(sys.argv[1]=="importmd5"):
+                hash_md5_import = hashcontroller.hashcontroller(str(sys.argv[2]))
+                hash_md5_import.import_md5_sql()
+
+            if(sys.argv[1]=="importsha256"):
+                hash_sha_256_import = hashcontroller.hashcontroller(str(sys.argv[2]))
+                hash_sha_256_import.import_sha256_sql()
+
+            if(sys.argv[1]=="importsha512"):
+                hash_sha_512_import = hashcontroller.hashcontroller(str(sys.argv[2]))
+                hash_sha_512_import.import_sha512_sql()
+
+
+
 
 
 
